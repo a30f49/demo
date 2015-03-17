f5b9ae8f1b9a3f781eeb1ec074dd240c46e04321
vincent@pc <vincent@jfeat.com>
2015-03-17 17:39:41 +0800

    init contentProvider

---
+++ contentprovider/src/main/java/com/sample/contentprovider/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sample.contentprovider.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.sample.contentprovider.app.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
