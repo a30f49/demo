58893a96f325e55fe4e9f0c9c752135b40104fe2
vincent@pc <vincent@jfeat.com>
2015-03-25 14:57:19 +0800

    test toolbar

--- actionbar/src/main/java/com/sample/actionbar/MainActivity.java
+++ actionbar/src/main/java/com/sample/actionbar/MainActivity.java
@@ -1,26 +1,63 @@
 package com.sample.actionbar;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final int SHOW_ICON_TOKEN = 1;
+    private static final int SHOW_LOGO_TOKEN = 2;
+    private static final int SHOW_AS_UP_TOKEN = 3;
+    private static final int SHOW_TITLE_CENTER_TOKEN = 4;
 
-    Toolbar mToolbar;
+    private int token = SHOW_TITLE_CENTER_TOKEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        ActionBar actionBar = getSupportActionBar();
 
-        setSupportActionBar(mToolbar);
+        switch (token) {
+            case SHOW_ICON_TOKEN:
+                // show icon
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+                actionBar.setIcon(R.drawable.ic_launcher);
+                break;
 
+            case SHOW_LOGO_TOKEN:
+                /// show logo
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+                actionBar.setLogo(R.drawable.abc_btn_radio_material);
+                break;
+
+            case SHOW_AS_UP_TOKEN:
+                /// show as up
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
+                actionBar.setIcon(R.drawable.ic_launcher);
+                break;
+
+            case SHOW_TITLE_CENTER_TOKEN:
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_CUSTOM);
+                actionBar.setLogo(R.drawable.abc_btn_radio_material);
+
+                LayoutInflater inflater = LayoutInflater.from(this);
+                View v = inflater.inflate(R.layout.custom_action_bar, null);
+                TextView title = (TextView) v.findViewById(R.id.title);
+                actionBar.setCustomView(v);
+                title.setText(R.string.app_title);
+                break;
+
+            default:break;
+        }
     }
 
 
@@ -39,7 +76,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_toolbar) {
+            Intent intent = new Intent(this, MainActivityToolbar.class);
+            startActivity(intent);
             return true;
         }
 
---
+++ actionbar/src/main/java/com/sample/actionbar/MainActivityToolbar.java
@@ -0,0 +1,44 @@
+package com.sample.actionbar;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivityToolbar extends ActionBarActivity {
+
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_toolbar);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mToolbar.setTitle(R.string.app_title);
+        mToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
