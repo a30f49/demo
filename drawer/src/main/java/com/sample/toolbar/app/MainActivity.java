14d5564e61d34d60e499c22c7b830b4921413b48
vincent <vincent@jfeat.com>
2015-06-29 08:03:17 +0800

    drawer picasso acra

--- drawer/src/main/java/com/sample/toolbar/app/MainActivity.java
+++ drawer/src/main/java/com/sample/toolbar/app/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
